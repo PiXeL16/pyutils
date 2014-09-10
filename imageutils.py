f635b8e5c0cbdd3f63254212fd8f73926911d88a
Chris <cjimenez@gmail.com>
2014-09-10 00:23:21 -0600

    Add exif utils

--- imageutils.py
+++ imageutils.py
@@ -2,6 +2,7 @@ import requests
 import io
 import logging
 from PIL import Image
+import exif
 import ioutils
 
 def search_images_in_google_with_text(search_text, ip_address):
@@ -58,17 +59,51 @@ def create_thumbnail(image, thumbnail_size=(250,250)):
 	image.thumbnail(thumbnail_size)
 	return image
 
+def resize_down_image(image, size):
+	"""
+	resize the image down to a specific size if its larger
+	"""
+	current_size = image.size();
+	if current_size > size:
+		image.rezise(size)
+
+	return image
+
+def save_image(image,image_name, folder_name):
+	"""
+	save the image with that name to a specific folder
+	"""
+	ioutils.check_or_create_folder(folder_name)
+	try:
+		image.save('%s/%s_thumb.jpg'%(folder_name,image_name), "JPEG")
+	except IOError as ex:
+		logging.error("cannot save image "+ image)
+		logging.exception(ex)
+
+def save_image_and_resize_down(image, size, image_name, folder_name):
+	"""
+	save the image with that name to a specific folder and resize it down if needed
+	"""
+	resized_image = resize_down_image(image, size)
+	save_image(resized_image, image_name, folder_name)
 	
 def save_image_thumbnail(image,thumbnail_size,image_name,folder_name):
 	'''
 	creates a thumbnail for the image with the name and in the folder path
 	'''
-	ioutils.check_or_create_folder(folder_name)
-	
 	try:
 		image_thumbnail = create_thumbnail(image,thumbnail_size)
-		image_thumbnail.save('%s/%s_thumb.jpg'%(folder_name,image_name), "JPEG")
+		save_image(image_thumbnail,image_name,folder_name)
 	except IOError as ex:
 		logging.error("cannot create thumbnail for "+ image)
 		logging.exception(ex)
-		
+
+def get_exif_field_from_image(image, exif_field):
+	"""
+	Gets the exif field from an image
+	"""
+	exif_values = exif.getexif(image)
+	k = list(exif_values.keys())
+	print k
+	if exif_field in exif_values:
+		return exif_values[exif_field]
