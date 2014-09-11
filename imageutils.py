4a5a5b87f06883269703f7b7930e4ed916c416ca
Chris <cjimenez@gmail.com>
2014-09-11 00:07:10 -0600

    Fix utils

--- imageutils.py
+++ imageutils.py
@@ -28,7 +28,7 @@ def search_images_in_google_with_text(search_text, ip_address):
 	'''
 
 	url = 'https://ajax.googleapis.com/ajax/services/search/images'
-	params = {'v': '1.0', 'userip': ip_address,'q':search_text}
+	params = {'v': '1.0','imgsz':'xlarge|large','userip': ip_address,'q':search_text}
 
 	json_response = requests.get(url, params=params).json();
 	
@@ -49,33 +49,37 @@ def get_image_from_url(image_url):
 		image = Image.open(image_file)
 		
 	except IOError as ex:
-		logging.error("cannot get image from url"+ image_url)
+		logging.error("cannot get image from url "+ image_url)
 		logging.exception(ex)
 	
 	return image
 
 def create_thumbnail(image, thumbnail_size=(250,250)):
 
-	image.thumbnail(thumbnail_size)
+	image.thumbnail(thumbnail_size,Image.ANTIALIAS)
 	return image
 
 def resize_down_image(image, size):
 	"""
 	resize the image down to a specific size if its larger
 	"""
-	current_size = image.size();
+	current_size = image.size;
 	if current_size > size:
-		image.rezise(size)
+		create_thumbnail(image,size)
 
 	return image
 
 def save_image(image,image_name, folder_name):
 	"""
 	save the image with that name to a specific folder
+	returns the saved image name with its extension
 	"""
+	image_saved_name = None
 	ioutils.check_or_create_folder(folder_name)
 	try:
-		image.save('%s/%s_thumb.jpg'%(folder_name,image_name), "JPEG")
+		save_name = '%s.jpg'%(image_name)
+		image.save('%s/%s'%(folder_name,save_name), "JPEG")
+		return save_name
 	except IOError as ex:
 		logging.error("cannot save image "+ image)
 		logging.exception(ex)
@@ -84,8 +88,13 @@ def save_image_and_resize_down(image, size, image_name, folder_name):
 	"""
 	save the image with that name to a specific folder and resize it down if needed
 	"""
-	resized_image = resize_down_image(image, size)
-	save_image(resized_image, image_name, folder_name)
+	try:
+		resized_image = resize_down_image(image, size)
+		image_saved_name = save_image(resized_image, image_name, folder_name)
+		return image_saved_name
+	except IOError as ex:
+		logging.error("cannot save image for "+ image)
+		logging.exception(ex)
 	
 def save_image_thumbnail(image,thumbnail_size,image_name,folder_name):
 	'''
@@ -93,7 +102,8 @@ def save_image_thumbnail(image,thumbnail_size,image_name,folder_name):
 	'''
 	try:
 		image_thumbnail = create_thumbnail(image,thumbnail_size)
-		save_image(image_thumbnail,image_name,folder_name)
+		image_saved_name = save_image(image_thumbnail,image_name,folder_name)
+		return image_saved_name
 	except IOError as ex:
 		logging.error("cannot create thumbnail for "+ image)
 		logging.exception(ex)
